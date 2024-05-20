4d6099a74bbc38c97f605daabd1211df6b259e56
Gabriel Marques <gabrielmarques1202@gmail.com>
2024-05-20 14:22:10 -0300

    tabela angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsuariosComponent } from './componente/usuarios/usuarios.component';
+import { initFlowbite } from 'flowbite';
+import { BuscaComponent } from "./componente/busca/busca.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, UsuariosComponent, BuscaComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tabelas';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
